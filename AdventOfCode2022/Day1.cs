aaa91bb8820419fdd9caa21054f971770e931306
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-12-01 22:36:09 +0100

    First day complete

--- AdventOfCode2022/Day1.cs
+++ AdventOfCode2022/Day1.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Reflection;
 
 namespace AdventOfCode2022
 {
@@ -6,7 +8,36 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            
+            string path = Path.Combine(Environment.CurrentDirectory, "data\\day1.txt");
+
+            string data = File.ReadAllText(path);
+
+            List<string> data_split = new List<string>(data.Split('\n'));
+
+            List<int> caloriesPerElf = new List<int>();
+
+            for (int i = 0; i < data_split.Count; i++)
+            {
+                int sum = 0;
+                while (data_split[i].Trim() != "")
+                {
+                    sum += int.Parse(data_split[i].Trim());
+                    i++;
+                }
+                caloriesPerElf.Add(sum);
+            }
+
+            Console.WriteLine(caloriesPerElf.Max());
+
+            int topThreeSum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int currentMax = caloriesPerElf.Max();
+                topThreeSum+= currentMax;
+                caloriesPerElf.Remove(currentMax);
+            }
+
+            Console.WriteLine(topThreeSum);
         }
     }
 }
